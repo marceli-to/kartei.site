cc24fff669b2641bbca111a0e19b513865a30191
Marcel Stadelmann <m@marceli.to>
2025-02-03 13:15:52 +0100

    wip

--- resources/js/spa/app.js
+++ resources/js/spa/app.js
@@ -5,14 +5,9 @@ import router from './router';
 
 const app = createApp(App);
 const pinia = createPinia();
-
 app.use(router);
 app.use(pinia);
 
-async function initializeApp() {
-  app.mount('#app');
-}
-
 if (document.getElementById('app')) {
-  initializeApp();
+  app.mount('#app');
 }
