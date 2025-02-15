1ea8ca961300879fdaca1bece2e0e201d9cc8143
Marcel Stadelmann <m@marceli.to>
2025-02-15 16:30:31 +0100

    wip

---
+++ resources/js/spa/components/buttons/ToggleGroup.vue
@@ -0,0 +1,77 @@
+<template>
+  <fieldset class="grid grid-cols-2 gap-8">
+    <label 
+      v-for="option in options" 
+      :key="option.value" 
+      class="relative">
+      <input
+        type="checkbox"
+        :name="name"
+        :value="option.value"
+        :checked="modelValue.includes(option.value)"
+        @change="updateValue(option.value)"
+        class="peer sr-only"
+      />
+      <span 
+        :class="[
+          defaultBorderClasses,
+          classes,
+          modelValue.includes(option.value) ? activeClasses : '',
+        ]"
+      >
+        {{ option.label }}
+      </span>
+    </label>
+  </fieldset>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  modelValue: {
+    type: Array,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  options: {
+    type: Array,
+    required: true,
+    validator: (value) => {
+      return value.every(option => 
+        typeof option === 'object' && 
+        'value' in option && 
+        'label' in option
+      );
+    }
+  },
+  classes: {
+    type: String,
+    default: 'min-h-default w-full flex items-center justify-center font-muoto-regular text-md text-graphite hover:text-black transition-all cursor-pointer px-8'
+  },
+  activeClasses: {
+    type: String,
+    default: 'bg-ice border-black !text-black'
+  }
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const defaultBorderClasses = 'border border-graphite';
+
+const updateValue = (value) => {
+  const newValue = [...props.modelValue];
+  const index = newValue.indexOf(value);
+  
+  if (index === -1) {
+    newValue.push(value);
+  } else {
+    newValue.splice(index, 1);
+  }
+  
+  emit('update:modelValue', newValue);
+};
+</script>
--- resources/js/spa/views/ui/Elements.vue
+++ resources/js/spa/views/ui/Elements.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid grid-cols-12 gap-48 mt-32">
-    <div class="col-span-6 flex flex-col gap-y-30">
+    <div class="col-span-4 flex flex-col gap-y-30">
       <div>
         <h2 class="mb-12 font-muoto-medium">Image Upload</h2>
         <ImageUpload
@@ -20,33 +20,8 @@
           ]" 
         />
       </div>
-      <div>
-        <h2 class="mb-12 font-muoto-medium">Toasts</h2>
-        <div class="flex gap-x-8">
-          <button 
-            @click="showToast('This is an info message!', 'info')"
-            class="bg-graphite text-white px-8 py-2 text-sm">
-            Show toast 'info' from script
-          </button>
-          <button 
-            @click="toast.show('This is a success message!', 'success')"
-            class="bg-graphite text-white px-8 py-2 text-sm">
-            Success
-          </button>
-          <button 
-            @click="toast.show('This is an error message!', 'error')"
-            class="bg-graphite text-white px-8 py-2 text-sm">
-            Error
-          </button>
-          <button 
-            @click="toast.show('This is an info message!', 'info')"
-            class="bg-graphite text-white px-8 py-2 text-sm">
-            Info
-          </button>
-        </div>
-      </div>
     </div>
-    <div class="col-span-6">
+    <div class="col-span-4">
       <h2 class="mb-12 font-muoto-medium">Buttons</h2>
       <div class="flex flex-col gap-y-32">
         <div>
@@ -109,20 +84,61 @@
             type="button"
             @click="showToast('This is an info message!', 'info')"
             :icon="{ name: 'Plus', position: 'center' }" />
-          />
+        </div>
+        <div>
+          <h3 class="mb-8 font-muoto-regular">Toggle Group</h3>
+          <ToggleGroup
+            v-model="selectedSections"
+            name="sections"
+            :options="sections" />
+
+            <template v-if="selectedSections.length">
+              <div class="bg-snow p-8 mt-12">
+                <h3 class="mb-4 font-muoto-regular">Selection</h3>
+                {{ selectedSections }}
+              </div>
+            </template>
+        </div>
+      </div>
+    </div>
+    <div class="col-span-4">
+      <div>
+        <h2 class="mb-12 font-muoto-medium">Toasts</h2>
+        <div class="flex gap-x-8">
+          <button 
+            @click="showToast('This is an info message!', 'info')"
+            class="bg-graphite text-white px-8 py-2 text-sm">
+            Show toast 'info' from script
+          </button>
+          <button 
+            @click="toast.show('This is a success message!', 'success')"
+            class="bg-graphite text-white px-8 py-2 text-sm">
+            Success
+          </button>
+          <button 
+            @click="toast.show('This is an error message!', 'error')"
+            class="bg-graphite text-white px-8 py-2 text-sm">
+            Error
+          </button>
+          <button 
+            @click="toast.show('This is an info message!', 'info')"
+            class="bg-graphite text-white px-8 py-2 text-sm">
+            Info
+          </button>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import ImageUpload from '@/components/ImageUpload.vue';
 import Slideshow from '@/components/Slideshow.vue';
 import ButtonGroup from '@/components/buttons/Group.vue';
 import ButtonPrimary from '@/components/buttons/Primary.vue';
 import ButtonAuth from '@/components/buttons/Auth.vue';
 import Action from '@/components/buttons/Action.vue';
+import ToggleGroup from '@/components/buttons/ToggleGroup.vue';
 import { useToastStore } from '@/store/toast';
 const toast = useToastStore();
 
@@ -132,4 +148,17 @@ onMounted(() => {
 function showToast(message = '', type = 'info') {
   toast.show(message, type)
 }
+
+const selectedSections = ref([]);
+const sections = [
+  { value: 'basic', label: 'Basisinformationen' },
+  { value: 'structure', label: 'Struktur' },
+  { value: 'template', label: 'Kartenvorlage' },
+  { value: 'tags', label: 'Tags' },
+  { value: 'users', label: 'Benutzer/innen' },
+  { value: 'export', label: 'Export' },
+  { value: 'share', label: 'Teilen' }
+];
+
+
 </script>
