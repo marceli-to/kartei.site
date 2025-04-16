db4b29e58e3455ccb3318ea9c9115b834801ab7c
Marcel Stadelmann <m@marceli.to>
2025-04-16 10:20:26 +0200

    fixes next/prev button issue with multiple sliders

--- resources/js/spa/components/media/Slideshow.vue
+++ resources/js/spa/components/media/Slideshow.vue
@@ -3,8 +3,8 @@
     <swiper
       :modules="modules"
       :navigation="{
-        prevEl: '.swiper-button-prev',
-        nextEl: '.swiper-button-next'
+        prevEl: prevButton,
+        nextEl: nextButton
       }"
       :loop="true"
       @swiper="onSwiper"
@@ -24,23 +24,30 @@
 
     </swiper>
 
-    <button class="swiper-button-prev absolute left-0 top-1/2 -translate-y-1/2 z-10 size-40 flex justify-center items-center">
+    <button :class="`swiper-button-prev swiper-button-prev-${uniqueId} absolute left-0 top-1/2 -translate-y-1/2 z-10 size-40 flex justify-center items-center`">
       <IconChevronLeft variant="small" />
     </button>
 
-    <button class="swiper-button-next absolute right-0 top-1/2 -translate-y-1/2 z-10 size-40 flex justify-center items-center">
+    <button :class="`swiper-button-next swiper-button-next-${uniqueId} absolute right-0 top-1/2 -translate-y-1/2 z-10 size-40 flex justify-center items-center`">
       <IconChevronRight variant="small" />
     </button>
 
   </div>
 </template>
 <script setup>
+import { ref } from 'vue'
+import { v4 as uuidv4 } from 'uuid'
 import { Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import 'swiper/css';
 import IconChevronLeft from '@/components/icons/ChevronLeft.vue';
 import IconChevronRight from '@/components/icons/ChevronRight.vue';
 import Image from '@/components/media/Image.vue';
+
+const uniqueId = uuidv4()
+const prevButton = `.swiper-button-prev-${uniqueId}`
+const nextButton = `.swiper-button-next-${uniqueId}`
+
 
 const props = defineProps({
   slides: {
